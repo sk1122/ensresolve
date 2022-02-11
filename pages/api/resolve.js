2b26acec69633b308477524cb71d61ba8c90325a
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-02-11 16:30:33 +0530

    Added Resolve API

--- pages/api/resolve.js
+++ pages/api/resolve.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ethers } from 'hardhat'
+import { ethers } from 'ethers'
 import ENS, { getEnsAddress } from '@ensdomains/ensjs'
 
 export default async function handler(req, res) {
